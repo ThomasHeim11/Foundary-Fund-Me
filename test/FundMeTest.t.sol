e7c290cf6928c9a8136fda45c483e6334baf4a9f
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-14 11:01:08 +0200

    update test and deploy script✅

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -13,6 +13,10 @@ contract FundMeTest is Test {
     }
 
     function testMinimumDollarIsFive() public {
-        assertEq(fundMe.MINIMUM_USD(),5e18);
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
