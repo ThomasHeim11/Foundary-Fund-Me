76aaa623d1aec25f579a01a6550535149cdfc28d
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-14 14:02:14 +0200

    update test and deploy contracts✅

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -81,4 +81,26 @@ contract FundMeTest is Test {
             endingOwnerBalance
         );
     }
+
+    function testWitdrawFromMultiplleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+            fundMe.getOwner().balance
+        );
+    }
 }
